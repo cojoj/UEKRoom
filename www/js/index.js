2d05099240390c436b0cb9cb3e8dfc8288e90e0f
Mckomo <mckomo@gmail.com>
2015-04-08 17:01:51 +0200

    Added basic functionality

--- www/js/index.js
+++ www/js/index.js
@@ -23,11 +23,38 @@ var app = {
         $.getJSON("http://devplan.uek.krakow.pl/api/groups", function(groups) {
             $.each(groups, function(index, group) {
                 // console.log(group.name);
-                $("#groups-list").append("<li>" + group.name + "</li>");
+                $("#groups-list").append('<li><a href="#" onclick="app.search(this)" data-id="' +  group.id + '">' + group.name + '</a></li>');
             })
             $("#groups-list").listview("refresh");
         });
+    },
+    search: function(group) {
+
+        var groupId = $(group).data('id'),
+            startDate = $('#start-date').val(),
+            endDate = $('#end-date').val(),
+            internalOnly = $('#internal-only').attr(':checked'),
+            includeLabs = $('#include-labs').attr(':checked');
+
+        if (!groupId || !startDate || !endDate) {
+            alert('Wypełnij wszystie pola');
+        }
+
+        var params = {
+            'group_id': [groupId],
+            'start_date': $.datepicker.formatDate("yy-mm-dd", new Date(startDate)),
+            'end_date': $.datepicker.formatDate("yy-mm-dd", new Date(endDate)),
+            'internal_only': internalOnly,
+            'include_labs': includeLabs
+        }
+
+        devPlan.availablePlaces(params, function(dates) {
+            calendar.bind($('#calendar'), dates);
+            $.mobile.changePage('#Result');
+        });
     }
 };
+
+
 
 app.initialize();
